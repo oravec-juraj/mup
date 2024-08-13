ed0ad320009a83afb6fcfb459545b81e253ed1e5
Juraj Oravec <juraj.oravec@stuba.sk>
2024-08-13 12:45:56 +0200

    R20240813

--- rmpc_methods/feas_kothare_soft_con.m
+++ rmpc_methods/feas_kothare_soft_con.m
@@ -5,34 +5,8 @@
 %
 %   juraj.oravec@stuba.sk
 %
-%   est.:2016.03.05.
 %
 
-% Copyright is with the following author(s):
-%
-% (c) 2016 Juraj Oravec, Slovak University of Technology in Bratislava,
-% juraj.oravec@stuba.sk
-% (c) 2016 Monika Bakosova, Slovak University of Technology in Bratislava,
-% monika.bakosova@stuba.sk
-% ------------------------------------------------------------------------------
-% Legal note:
-% This program is free software; you can redistribute it and/or
-% modify it under the terms of the GNU General Public
-% License as published by the Free Software Foundation; either
-% version 2.1 of the License, or (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-% General Public License for more details.
-%
-% You should have received a copy of the GNU General Public
-% License along with this library; if not, write to the
-% Free Software Foundation, Inc.,
-% 59 Temple Place, Suite 330,
-% Boston, MA 02111-1307 USA
-%
-% ------------------------------------------------------------------------------
 
 function [chk,chk_eig] = feas_kothare_soft_con(design,model,results,setup)
 
